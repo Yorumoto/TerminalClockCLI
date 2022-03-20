bb48c5c98f1b4284635977c454fbba696a7ab920
mitsupishi <no>
2022-03-20 19:14:44 +0800

    yay

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import curses
 import time
 import logging
+import json
 from database import letters, padding, letter_height
 logging.basicConfig(filename='.log', filemode='w+', format="%(levelname)s: %(message)s", level=logging.DEBUG)
 
+with open('settings.json', 'r') as f:
+    user_settings = json.loads(f.read())
+
 last_text = ""
 last_terminal_size = (0, 0)
+
+class Filler:
+    ON = curses.A_REVERSE
+    OFF = curses.A_NORMAL
+
+if user_settings['reversed']:
+    Filler.ON, Filler.OFF = Filler.OFF, Filler.ON
 
 def render(src, new):
     global last_text, cleared, last_terminal_size
@@ -15,6 +26,10 @@ def render(src, new):
     y -= letter_height // 2
     x -= sum([((len(letters[x]) // letter_height) // 2) + padding for x in new if x in letters])
 
+    if last_terminal_size != terminal_size:
+        src.clear()
+        last_text = ""
+    
     if len(new) > len(last_text):
         for _ in range(len(new)):
             last_text += " "
@@ -31,7 +46,7 @@ def render(src, new):
             tx = x + (index // letter_height)
             
             if item != last:
-                src.addstr(ty, tx, " ", curses.A_REVERSE if signal > 0 else curses.A_NORMAL)
+                src.addstr(ty, tx, " ", Filler.ON if signal > 0 else Filler.OFF)
                 src.timeout(1)
                 src.getch()
             
@@ -46,11 +61,19 @@ def render(src, new):
 
 CURSES_ERROR_MESSAGE = "Cannot render in this size"
 
+def center_text(src, text):
+    y, x = (x//2 for x in src.getmaxyx())
+    x -= len(CURSES_ERROR_MESSAGE) // 2
+    src.clear()
+    src.addstr(y, x, "Cannot render in this size")
+    src.refresh()
+
 def main(src):
     err = True
+    fmt = f"{'%H' if not user_settings['ampm'] else '%I'}:%M:%S"
 
     while True:
-        text = time.strftime('%H:%M:%S')
+        text = time.strftime(fmt)
         try:
             if err:
                 src.nodelay(True)
@@ -61,11 +84,7 @@ def main(src):
             err = True
             src.nodelay(False)
             try:
-                y, x = (x//2 for x in src.getmaxyx())
-                x -= len(CURSES_ERROR_MESSAGE) // 2
-                src.clear()
-                src.addstr(y, x, "Cannot render in this size")
-                src.refresh()
+                center_text(src, "Cannot render in this size")
             except curses.error:
                 pass
             src.getch()
